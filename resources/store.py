767699483e334b4f536cc767f668a6553146b843
Ian Ferguson <ian@IanFergusonsAir.gateway>
2024-04-15 23:38:01 +1000

    general improvements

--- resources/store.py
+++ resources/store.py
@@ -1,8 +1,8 @@
 import uuid
-from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from db import stores
+from schemas import StoreSchema
 
 
 blp = Blueprint("Stores", "stores", description="Operations on stores")
@@ -10,7 +10,8 @@ blp = Blueprint("Stores", "stores", description="Operations on stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
-    def get(self, store_id):
+    @blp.response(200, StoreSchema)
+    def get(cls, store_id):
         try:
             # You presumably would want to include the store's items here too
             # More on that when we look at databases
@@ -18,7 +19,7 @@ class Store(MethodView):
         except KeyError:
             abort(404, message="Store not found.")
 
-    def delete(self, store_id):
+    def delete(cls, store_id):
         try:
             del stores[store_id]
             return {"message": "Store deleted."}
@@ -28,16 +29,13 @@ class Store(MethodView):
 
 @blp.route("/store")
 class StoreList(MethodView):
-    def get(self):
-        return {"stores": list(stores.values())}
+    @blp.response(200, StoreSchema(many=True))
+    def get(cls):
+        return stores.values()
 
-    def post(self):
-        store_data = request.get_json()
-        if "name" not in store_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'name' is included in the JSON payload.",
-            )
+    @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
+    def post(cls, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
                 abort(400, message=f"Store already exists.")
