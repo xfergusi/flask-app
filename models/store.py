9ac8d5dba8fdc143981dc00f44941161c840bbdf
Ian Ferguson <ian@Ian-Fergusons-MacBook-Air.local>
2024-04-16 13:25:07 +1000

    general improvements

--- models/store.py
+++ models/store.py
@@ -1,10 +1,10 @@
 from db import db
 
 
-class ItemModel(db.Model):
-    __tablename__ = "items"
+class StoreModel(db.Model):
+    __tablename__ = "stores"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False)
-    price = db.Column(db.Float(precision=2), unique=False, nullable=False)
-    store_id = db.Column(db.Integer, unique=False, nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+    items = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
